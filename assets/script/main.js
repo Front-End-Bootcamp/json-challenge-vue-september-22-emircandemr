67422abf5e21771933512a3ac3f0cefb103240cb
emircandemr <72731296+emircandemr@users.noreply.github.com>
2022-09-11 19:58:31 +0300

    final assignment

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,1 +1,36 @@
-console.log('👻');
+import groupData from "./data.json" assert {type: 'json'};
+
+const getGroups = (groupname) => {
+    
+// Data classified to group names   
+    let filterName = "group";
+    let filterByGroupName = groupData.reduce( (acc, item) => { 
+        let groupName = item[filterName];
+        if(!acc[groupName]){
+        acc[groupName] = [];  
+        }
+        acc[groupName].push(item);
+    return acc
+    } , [])
+
+//Classification process according to the desired group name
+    const getGroup = (key) => {
+        return {
+            [key] : {
+                members : filterByGroupName[key].map( (item) => item.type !== "assistant" ? item.name : null).filter( (item) => item !== null),
+                asistant : filterByGroupName[key].map( (item) => item.type === "assistant" ? item.name : null).filter( (item) => item !== null)
+                }
+        }
+    }
+
+    let groupList =(groupname ? getGroup(groupname) : Object.keys(filterByGroupName).map((key) => getGroup(key)))
+
+    console.log(groupList)
+
+}
+
+getGroups("IndianRed");
+getGroups();
+
+
+
