64736517d3c2ed13374eee60bd574ae59efe0edb
emircandemr <72731296+emircandemr@users.noreply.github.com>
2022-09-13 20:40:10 +0300

    Fixed getGroups and getGroup func.

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,36 +1,29 @@
 import groupData from "./data.json" assert {type: 'json'};
 
-const getGroups = (groupname) => {
-    
-// Data classified to group names   
-    let filterName = "group";
-    let filterByGroupName = groupData.reduce( (acc, item) => { 
-        let groupName = item[filterName];
-        if(!acc[groupName]){
-        acc[groupName] = [];  
-        }
-        acc[groupName].push(item);
-    return acc
-    } , [])
-
-//Classification process according to the desired group name
-    const getGroup = (key) => {
-        return {
-            [key] : {
-                members : filterByGroupName[key].map( (item) => item.type !== "assistant" ? item.name : null).filter( (item) => item !== null),
-                asistant : filterByGroupName[key].map( (item) => item.type === "assistant" ? item.name : null).filter( (item) => item !== null)
-                }
-        }
-    }
-
-    let groupList =(groupname ? getGroup(groupname) : Object.keys(filterByGroupName).map((key) => getGroup(key)))
-
-    console.log(groupList)
-
+const getGroups = () => {
+    let classifiedByGroupName = groupData.reduce((groups, student) => {   
+        let groupName = student["group"];  // Group names taken to classify student groups by group names  
+        if(!groups[groupName]){  // If the group name is not in the groups object, add it 
+            groups[groupName] = { // An object has been created to classify members according to their groups 
+            members: [] ,  // An array has been created to classify members according to their groups 
+            assistant: [], 
+        }}
+        groups[groupName] = { 
+            members: [...groups[groupName].members, student.name],  // The name of the student is added to the members array of the group object
+            assistant: student.type === "assistant" ? student.name : [] }; // If the student is an assistant, the name of the assistant is added to the assistant array of the group object
+            return groups 
+        }, {} )
+    return classifiedByGroupName // The object that classifies the members according to their groups is returned
 }
 
-getGroups("IndianRed");
-getGroups();
-
-
+const getGroup = (groupName) => {
+    let classifiedByGroupName = getGroups(); // The object that classifies the members according to their groups is taken
+    let filterByGroupName = { // An object is created to filter the members by the desired group name.
+        [groupName] : 
+            classifiedByGroupName[groupName] // The members of the desired group name are taken from the object that classifies the members according to their groups.
+        }
+    return filterByGroupName // The object that filters the members by the desired group name is returned
+}
+console.log(getGroups());
+console.log(getGroup("Orchid"));
 
